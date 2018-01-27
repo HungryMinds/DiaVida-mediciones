19492c96eac058cd4371cfdc44d2488395d9cdf0
DaveCodes <david@codeskill.io>
2018-01-27 10:36:32 -0600

    CRJ2-Login Add Inputs - Adds material inputs - Adds routes login & notfound - Add forms module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material';
 
 // App Imports
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { AppRoutingModule } from './app.routing';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 import { environment } from '../environments/environment';
+import { NotfoundComponent } from './notfound/notfound.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, LoginComponent, NotfoundComponent],
   imports: [
     BrowserModule,
-    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
+    ServiceWorkerModule.register('/ngsw-worker.js', {
+      enabled: environment.production
+    }),
     BrowserAnimationsModule,
     // AuthModule,
     // CoreModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
     AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,11 +2,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+// @Components
+import { LoginComponent } from './login/login.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+
 const appRoutes = [
   // { path: 'requests', loadChildren: 'app/request/request.module#RequestModule' },
   // { path: 'admin', loadChildren: 'app/admin/admin.module#AdminModule' },
+  { path: 'login', component: LoginComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: '**', redirectTo: '/login' }
+  { path: '**', component: NotfoundComponent }
 ];
 
 @NgModule({
