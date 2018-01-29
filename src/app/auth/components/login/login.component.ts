6547ddbafb1b705eab8999609afe03a041739afa
DaveCodes <david@codeskill.io>
2018-01-29 06:39:49 -0600

    CRJ2-Login-design Implements design - Adds assets - Adds styling - Add roboto font and basic reset of global styles

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -5,16 +5,47 @@ import {
   FormGroup,
   Validators
 } from '@angular/forms';
+import {
+  trigger,
+  transition,
+  style,
+  animate,
+  query,
+  stagger,
+  animateChild,
+  keyframes,
+  state
+} from '@angular/animations';
 
 @Component({
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  animations: [
+    trigger('grow', [
+      state('small', style('*')),
+      state('large', style({ height: 'auto' })),
+      transition('small => large', animate('100ms ease-in')),
+      transition('large => small', animate('100ms ease-out'))
+    ]),
+    trigger('showForm', [
+      state('small', style({ opacity: 0 })),
+      state('large', style({ opacity: 1 })),
+      transition('small => large', animate('100ms ease-in')),
+      transition('large => small', animate('100ms ease-out'))
+    ])
+  ]
 })
 export class LoginComponent {
   public loginForm: FormGroup;
 
   constructor(private fb: FormBuilder) {
     this.createForm();
+  }
+
+  state = 'small';
+
+  animateMe() {
+    this.state = this.state === 'small' ? 'large' : 'small';
   }
 
   createForm() {
