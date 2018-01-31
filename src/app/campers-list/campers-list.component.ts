15988f9f03eea935369c36e4dc71c75c52398ff3
Angel Echavarria <angel@interaction.cr>
2018-01-30 23:21:42 -0600

    wip

--- src/app/campers-list/campers-list.component.ts
+++ src/app/campers-list/campers-list.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { MatTableDataSource } from '@angular/material';
 
 @Component({
   selector: 'app-campers-list',
   templateUrl: './campers-list.component.html',
   styleUrls: ['./campers-list.component.scss']
 })
-export class CampersListComponent implements OnInit {
+export class CampersListComponent {
+  displayedColumns = ['', 'Nombre', 'Edad'];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
 
-  constructor() { }
-
-  ngOnInit() {
+  applyFilter(filterValue: string) {
+    filterValue = filterValue
+      .trim() // Remove whitespace
+      .toLowerCase(); // MatTableDataSource defaults to lowercase matches
+    this.dataSource.filter = filterValue;
   }
-
 }
+
+export interface CamperTable {
+  group: string;
+  name: string;
+  age: number;
+}
+
+const ELEMENT_DATA: CamperTable[] = [
+  {group: 'morado', name: 'Manuel Antonio', age: 13},
+  {group: 'morado', name: 'Manuel Antonio', age: 13},
+  {group: 'morado', name: 'Manuel Antonio', age: 13},
+  {group: 'morado', name: 'Manuel Antonio', age: 13},
+  {group: 'morado', name: 'Manuel Antonio', age: 13},
+  {group: 'morado', name: 'Manuel Antonio', age: 13},
+  {group: 'morado', name: 'Manuel Antonio', age: 13},
+  {group: 'morado', name: 'Manuel Antonio', age: 13},
+  {group: 'morado', name: 'Manuel Antonio', age: 13},
+  {group: 'morado', name: 'Manuel Antonio', age: 13},
+  {group: 'morado', name: 'Manuel Antonio', age: 13},
+];
