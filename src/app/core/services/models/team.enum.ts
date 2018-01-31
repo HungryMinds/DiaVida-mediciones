b36e4e95d98f24fd7165b5ae18a416cf76bb1e21
Angel Echavarria <aechavarria@lacreativeria.com>
2018-01-31 08:51:35 -0600

    fix in models

--- src/app/core/services/models/team.enum.ts
+++ src/app/core/services/models/team.enum.ts
@@ -1,7 +1,7 @@
 export enum Team {
-  morado = '',
-  verde = '',
-  azul = '',
-  amarillo = '',
-  rojo = ''
+  morado = '#7A00E0',
+  verde = '#43E021',
+  azul = '#2189E0',
+  amarillo = '#E0D313',
+  rojo = '#E01612'
 }
