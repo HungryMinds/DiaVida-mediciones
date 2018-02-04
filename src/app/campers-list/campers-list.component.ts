9ed3ba5934c25437dedd76df191eaacbedd5b68d
Angel Echavarria <echavarriasam@gmail.com>
2018-02-04 15:41:54 -0600

    CRJ3-camper_list list working awesomely

--- src/app/campers-list/campers-list.component.ts
+++ src/app/campers-list/campers-list.component.ts
@@ -2,15 +2,35 @@
 import { Component } from '@angular/core';
 import { MatTableDataSource } from '@angular/material';
 import { Team } from '../core/services/models/team.enum';
+import { CampistService } from '../core/services/campist.service';
+import { Campist } from '../core/services/models/campist.class';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
   selector: 'app-campers-list',
   templateUrl: './campers-list.component.html',
   styleUrls: ['./campers-list.component.scss']
 })
-export class CampersListComponent {
+export class CampersListComponent implements OnInit {
   displayedColumns = ['group', 'name', 'age'];
-  dataSource = new MatTableDataSource(ELEMENT_DATA);
+  dataSource;
+
+  constructor(private cL: CampistService) {}
+
+  ngOnInit() {
+    let campists = [];
+    this.cL.getCampists()
+    .forEach((_campists) => {
+      campists = _campists.map((campist) => {
+        return {
+          group: Team[campist.team],
+          name: `${campist.names} ${campist.lastNames}`,
+          age: campist.age
+        };
+      });
+      this.dataSource = new MatTableDataSource(campists);
+    });
+  }
 
   applyFilter(filterValue: string) {
     filterValue = filterValue
