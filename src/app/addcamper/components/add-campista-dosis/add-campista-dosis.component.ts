ada08231e13ac1c4d36a39ce6392ab0c10b5411f
DaveCodes <david@codeskill.io>
2018-02-04 20:39:20 -0600

    adding logic /dosis

--- src/app/addcamper/components/add-campista-dosis/add-campista-dosis.component.ts
+++ src/app/addcamper/components/add-campista-dosis/add-campista-dosis.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import {
+  FormControl,
+  FormBuilder,
+  FormGroup,
+  Validators
+} from '@angular/forms';
 
 @Component({
   selector: 'app-add-campista',
@@ -13,21 +19,50 @@ export class AddCampistaDosisComponent implements OnInit {
   isChecked: Boolean = false;
   @Input() checked;
   @Output() change: EventEmitter<boolean> = new EventEmitter<boolean>();
+  url = 'camper/add-camper/';
+  nextUrl = 'dosis';
+  public dosisForm: FormGroup;
   camper: any;
 
-  constructor(private route: ActivatedRoute, private router: Router) {
+  constructor(
+    private fb: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
     this.title = 'Agregar Campista';
     this.subtitle = 'Dosis Basal';
+
+    this.createForm();
+  }
+
+  createForm() {
+    this.dosisForm = this.fb.group({
+      time: ['', Validators.required],
+      dosis: ['', Validators.required],
+      'second-time': [''],
+      'second-dosis': ['']
+    });
   }
 
   toogleView(event) {
     this.isChecked = event.checked;
   }
 
+  next(event) {
+    event.preventDefault();
+    this.camper = { ...this.camper, ...this.dosisForm.value };
+    console.table(this.camper);
+    // Navigate to the next view
+    this.router.navigate([this.url + this.nextUrl, this.camper]);
+  }
+
+  goBack(event) {
+    event.preventDefault();
+  }
+
   ngOnInit() {
     this.route.params.subscribe(params => {
       this.camper = params;
-      console.log('CAMPER', this.camper);
     });
   }
 }
