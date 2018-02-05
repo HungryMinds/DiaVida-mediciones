65f666518bc614126728e4e389fd53ede77a0531
jescalante <jescalante@lacreativeria.com>
2018-02-05 09:07:18 -0600

    CRJ4 - added function and flag to extend delete button on click

--- src/app/camperdetail/camperdetail.component.ts
+++ src/app/camperdetail/camperdetail.component.ts
@@ -9,12 +9,31 @@ export class CamperdetailComponent implements OnInit {
   aditionalMedication: string
   allergies: string
   basalInsulin: string
+  errorButtonMessage: string
+  errorButtonCheck: boolean
 
   constructor() { 
     this.aditionalMedication = 'Beclometasona en inhalador';
     this.allergies = 'Abejas, queso y mariscos';
     this.basalInsulin = 'No necesita insulina basal';
+    this.errorButtonMessage = 'ELIMINAR';
+    this.errorButtonCheck = false;
   }
+
+  deleteCamper(id) {
+    if (!this.errorButtonCheck) {
+      this.errorButtonMessage = 'ELIMINAR EL CAMPISTA';
+      // TODO: Por ahora lo quita para probar      
+      this.errorButtonCheck = true;  
+    } else {
+      console.log('Delete camper Function!')
+      // TODO: Por ahora lo quita para probar
+      this.errorButtonMessage = 'ELIMINAR';
+      this.errorButtonCheck = false;        
+    }
+  }
+
+  
 
   ngOnInit() {
   }
