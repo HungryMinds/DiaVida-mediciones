abc5b4c091459c0abc4d30eb5e3a404bb3782808
Angel Echavarria <echavarriasam@gmail.com>
2018-02-08 12:00:24 -0600

    added firebase dev in config file for development

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,9 +2,9 @@
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
-import { firebase } from './firestore';
+import { firebase, firebaseDev } from './firestore';
 
 export const environment = {
   production: false,
-  firebase
+  firebase: firebaseDev
 };
